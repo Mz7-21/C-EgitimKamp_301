27b9ee83dda271b5a7d7e6ecbe75a75d894983e1
Mz7-21 <ylmzlm17@gmail.com>
2025-10-24 21:11:25 +0300

    Add files via upload

---
+++ FrmProduct.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary1C_EgitimKamp_301_BusinessLayer.Abstract;
+using ClassLibrary1C_EgitimKamp_301_BusinessLayer.concrete;
+using ClassLibrary1C_EgitimKamp_301_DataAccessLayer.Abstract;
+using ClassLibrary1C_EgitimKamp_301_DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_EgitimKamp_301.PresentionalLayers
+{
+    public partial class FrmProduct : Form
+    { //ProductManager ProductManager = new ProductManager(new EfProductDal());
+        private readonly IProductService _productService;
+
+        public FrmProduct()
+        {
+            _productService = new ProductManager(new EfProductDal());
+            InitializeComponent();
+        }
+
+   private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetAll();
+            dataGridView1.DataSource = values;
+        }
+    }
+}
